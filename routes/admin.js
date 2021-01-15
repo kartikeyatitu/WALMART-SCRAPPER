4838473656ff0d5e213a92c89d2f25860fd4f8e8
kartikeyatitu <kartikeya2998@gmail.com>
2021-01-16 01:55:42 +0530

    kk

--- routes/admin.js
+++ routes/admin.js
@@ -19,7 +19,7 @@ function isAuthenticatedUser(req, res, next) {
 
 async function scrapeData(url, page) {
     try {
-        await page.goto(url, {waitUntil: 'domcontentloaded', timeout : 0});
+        await page.goto(url);
 
         
         const html = await page.evaluate(() => document.body.innerHTML);
